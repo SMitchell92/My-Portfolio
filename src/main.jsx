8453cf8f6830007ab05fe0b7189555b475b6b4e5
StuMitch <stuart.mitchell92@gmail.com>
2025-01-28 17:57:54 -0800

    Laid basic framework out or made skeleton

---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import ReactDOM from 'react-dom/client'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App';
+import Error from './pages/Error';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Resume from './pages/Resume';
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <Error />,
+      children: [
+        {
+          index: true,
+          element: <About />,
+        },
+        {
+          path: '/Portfolio',
+          element: <Portfolio />,
+        },
+        {
+          path: '/Contact',
+          element: <Contact />,
+        },
+        {
+          path: '/Resume',
+          element: <Resume />,
+        },
+      ],
+    },
+  ]);
+  
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+  );
+  
