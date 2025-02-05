5da58b273ce22bc3801144b4752ec7b9b2aeb5de
StuMitch <stuart.mitchell92@gmail.com>
2025-02-05 13:39:57 -0800

    fixed link issues in portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,12 +4,7 @@ export default function Portfolio() {
     return (
       <div id="nav-page-container">
         <h1>Portfolio</h1>
-<img className="card" id="portfolioPNG" image="employeeTracker.png" link="https://github.com/SMitchell92/Employee-Tracker/" />
-<img className="card" id="portfolioPNG" image="weatherForecast.png" link="https://weather-forecast-api-caller.onrender.com/" />
-<img className="card" id="portfolioPNG"image="Vehicle_Depot.png" link="https://github.com/SMitchell92/Vehicle-Depot" />
-<img className="card" id="portfolioPNG" image="READMEGenerator.png" link="https://github.com/SMitchell92/READMEGenerator9000" />
-<img className="card" id="portfolioPNG" image="preworkStudyGuide.png" link="https://github.com/SMitchell92/prework-study-guide" />
-<img className="card" id="portfolioPNG" image="Mood_Playlist_Generator.png" link="https://github.com/aelias-thepacer/mood_playlist_generator" />
+
 <Project image="preworkStudyGuide.png" link="https://github.com/SMitchell92/prework-study-guide"/>
 <Project image="READMEGenerator.png" link="https://github.com/SMitchell92/READMEGenerator9000"/>
 <Project image="Vehicle_Depot.png" link="https://github.com/SMitchell92/Vehicle-Depot"/>
@@ -18,5 +13,5 @@ export default function Portfolio() {
 <Project image="weatherForecast.png" link="https://weather-forecast-api-caller.onrender.com/"/>
       </div>
     );
-  }
+}
   
