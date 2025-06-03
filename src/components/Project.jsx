ee23cb8eec19288ccd952f47bac46c25f5fc9dff
StuMitch <stuart.mitchell92@gmail.com>
2025-06-03 15:38:11 -0700

    fixed project image display issue

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,13 +4,12 @@ const { name, description, link, image } = props;
 
 return (
   <a href={link} className="btn btn-primary">
-  <div className="card">
     <img src={image} className="card-img-top" alt={name} />
     <div className="card-body">
       <h5 className="card-title">{name}</h5>
       <p className="card-text">{description}</p>
     </div>
-  </div>
+
   </a>
 );
 }
